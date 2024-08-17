7393f1fca1010b7caf26cfc7e335faee3ae1631c
Reon Uemura <106325619+leonuemura@users.noreply.github.com>
2024-08-18 01:44:21 +0900

    Update config.h

--- config.h
+++ config.h
@@ -6,7 +6,7 @@
 
 const std::string data_name = "dataset/data.csv";
 const std::string query_name = "dataset/query.csv";
-const std::string weight_name = "dataset/weights.csv";
+const std::string weight_name = "dataset/weight.csv";
 const int graph_k = 20; //hp*graph_k is the number of outedges for each node
 const int search_k = 10; //searching parameter
 const int kmeans_chooselimit = 10; //the number of iterations of k-means
